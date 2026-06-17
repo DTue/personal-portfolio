7bae0576387b603a475339310b68c1c23b52a939
Tue(Dolphin) Tran <tuetamtran2307@gmail.com>
2026-06-16 21:02:07 -0700

    Add: sections and projects outline

--- personal-portfolio/src/App.jsx
+++ personal-portfolio/src/App.jsx
@@ -1,16 +1,28 @@
 import './App.css'
 
-
+const projects = [
+  "Hackathons",
+  "Academic Projects",
+  "Media Projects"
+]; 
 
 
 function App() {
   return (
-    <div> 
+    <section> 
     <h1>Dolphin's Digital Portfolio</h1>
     <h2>About Me</h2>
+
     <h2>My Projects</h2>
+
+    { 
+    projects.map(project => (
+      <p>{project}</p>
+    ))
+    }
+
     <h2>Let's Connect</h2>
-   </div> 
+   </section> 
    
   ); 
 }
