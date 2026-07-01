80a5b9b0e1e5f1c64cf6d462d8499c56f583193d
Tue(Dolphin) Tran <tuetamtran2307@gmail.com>
2026-06-30 23:31:19 -0700

    Fixed: Biography section

--- personal-portfolio/src/components/About.jsx
+++ personal-portfolio/src/components/About.jsx
@@ -2,11 +2,12 @@ function About(){
     return(
         <section className="biography-section">
             <p>
-                Tue “Dolphin” Tran is a third-year undergraduate student majoring in Computer Science at San Diego State University. 
-                She strives to understand how systems are designed, who they serve, and ways to ethically leverage computer science to create user-centered and accessible solutions. 
-                Her research interests lie at the intersection of computer-human interaction and computational science, with a particular focus on healthcare and community-based applications. 
-                Drawing from her international background, Dolphin values interdisciplinary collaboration and integrates global perspectives with data-driven approaches to technology. Her work spans across low-barrier AI-assisted communication systems, healthcare accessibility, and socio-environmental computational analysis. 
-                For future goals, Dolphin hopes to pursue a Ph.D in human-computer interaction and an interdisciplinary research-informed career path, ultimately fostering research collaborations between institutions across the United States, Vietnam, and the broader international community. 
+                Hi, thank you for your time visiting my page! My name is Tue Tran. I also go by Dolphin Tran. I am a third-year undergraduate student majoring in Computer Science at San Diego State University. 
+                I strive to understand how systems are designed, who they serve, and ways to ethically leverage computer science to create user-centered and accessible solutions. 
+                My research interests lie at the intersection of computer-human interaction and computational science, with a particular focus on healthcare and community-based applications. 
+                Drawing from my background as an international student from Vietnam, I value interdisciplinary collaboration and integrate global perspectives with data-driven approaches to technology.
+                 My work spans across low-barrier AI-assisted communication systems, healthcare accessibility, and socio-environmental computational analysis. 
+                For future goals, I hope to pursue a Ph.D in human-computer interaction and an interdisciplinary research-informed career path, ultimately fostering research collaborations between institutions across the United States, Vietnam, and the broader international community. 
              </p>
         </section>
 
