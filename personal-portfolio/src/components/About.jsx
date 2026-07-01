c3a042882951a4d92e6d77223719d50b580fee21
Tue(Dolphin) Tran <tuetamtran2307@gmail.com>
2026-06-30 23:29:56 -0700

    Fixed: About section content with new biography

--- personal-portfolio/src/components/About.jsx
+++ personal-portfolio/src/components/About.jsx
@@ -2,10 +2,11 @@ function About(){
     return(
         <section className="biography-section">
             <p>
-                Tue “Dolphin” Tran is a second-year international undergraduate student from Vietnam pursuing a Bachelor of Science in Computer Science at San Diego State University. 
-                She is passionate about developing human-centered technological solutions through STEM and interdisciplinary collaboration. 
-                Drawing from her international background, Dolphin integrates global perspectives, multimedia storytelling, and data-driven problem-solving with adaptive leadership. 
-                Her interests lie at the intersection of computer science and healthcare, where she aims to explore computational systems and ethical technologies that support underrepresented and globally diverse communities
+                Tue “Dolphin” Tran is a third-year undergraduate student majoring in Computer Science at San Diego State University. 
+                She strives to understand how systems are designed, who they serve, and ways to ethically leverage computer science to create user-centered and accessible solutions. 
+                Her research interests lie at the intersection of computer-human interaction and computational science, with a particular focus on healthcare and community-based applications. 
+                Drawing from her international background, Dolphin values interdisciplinary collaboration and integrates global perspectives with data-driven approaches to technology. Her work spans across low-barrier AI-assisted communication systems, healthcare accessibility, and socio-environmental computational analysis. 
+                For future goals, Dolphin hopes to pursue a Ph.D in human-computer interaction and an interdisciplinary research-informed career path, ultimately fostering research collaborations between institutions across the United States, Vietnam, and the broader international community. 
              </p>
         </section>
 
